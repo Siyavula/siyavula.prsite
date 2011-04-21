6686756f5e2e1a9915c42488443f81566479a7ad
Johan Beyers <jbeyers@juizi.com>
2011-04-21 06:32:55 +0200

    Basic book content type

---
+++ src/siyavula.app/siyavula/app/book.py
@@ -0,0 +1,34 @@
+from five import grok
+from zope import schema
+
+from plone.directives import form, dexterity
+
+from plone.app.textfield import RichText
+from plone.namedfile.field import NamedImage
+
+from siyavula.app import _
+
+class IBook(form.Schema):
+    """A book.
+    """
+    
+    title = schema.TextLine(
+            title=_(u"Title"),
+        )
+    
+    description = schema.Text(
+            title=_(u"A short summary"),
+        )
+    
+    content = RichText(
+            title=_(u"Content"),
+            description=_(u"A longer description of the book"),
+            required=False
+        )
+    
+    cover = NamedImage(
+            title=_(u"Cover Image"),
+            description=_(u"Please upload an image"),
+            required=False
+        )
+
