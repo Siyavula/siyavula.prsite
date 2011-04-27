04dafb115a9ccc8a9301b9d45e2d99d43726ab1e
Johan Beyers <jbeyers@juizi.com>
2011-04-27 12:56:28 +0200

    Initial site structure gets created automatically

--- src/siyavula.app/siyavula/app/setuphandlers.py
+++ src/siyavula.app/siyavula/app/setuphandlers.py
@@ -11,18 +11,47 @@ def setupVarious(context):
 
     # Create a few top-level folders for the content.
     portal = context.getSite()
-    if not portal.hasObject('books'):
-        portal.invokeFactory(type_name='siyavula.app.section', id='books',
-            title='WebBooks') 
-        section = portal._getOb('books')
 
-        wf = getToolByName(portal, 'portal_workflow')
-        wf.doActionFor(section,'publish')
-        section.reindexObject()
+    sections = [
+        {'id': 'books',
+        'title': 'WebBooks',
+        'view': 'booksview'},
+        {'id': 'other',
+        'title': 'Other Products',
+        'view': 'view'},
+        {'id': 'volunteers',
+        'title': 'Volunteers',
+        'view': 'volunteersview'},
+        {'id': 'courses',
+        'title': 'Courses',
+        'view': 'coursesview'},
+        {'id': 'blog',
+        'title': 'Blog',
+        'view': 'blogview'},
+        {'id': 'about',
+        'title': 'About',
+        'view': 'view'},
+        {'id': 'contact',
+        'title': 'Contact',
+        'view': 'view'},
+    ]
 
-        # The next line enables a different view.
-        section.setLayout('booksview')
-        # stop anyone from changing the display of this folder
+    for section_dict in sections:
+        if not portal.hasObject(section_dict['id']):
+            portal.invokeFactory(type_name='siyavula.app.section', id=section_dict['id'],
+                title=section_dict['title']) 
+            section = portal._getOb(section_dict['id'])
+
+            wf = getToolByName(portal, 'portal_workflow')
+            wf.doActionFor(section,'publish')
+            section.reindexObject()
+
+        else:
+            section = portal._getOb(section_dict['id'])
+
+        # Set the default view.
+        section.setLayout(section_dict['view'])
+        # Stop anyone from changing the display of this folder
         section.manage_permission(ModifyViewTemplate, roles=[])
-        # stop people from change the allowed types
+        # Stop people from changing the allowed types
         section.manage_permission(ModifyConstrainTypes, roles=[])
