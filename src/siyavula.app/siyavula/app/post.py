ab292ef3383e4ce8141ca579b1518a1310f1d254
Johan Beyers <jbeyers@juizi.com>
2011-04-27 12:52:26 +0200

    Post dexterity type added

---
+++ src/siyavula.app/siyavula/app/post.py
@@ -0,0 +1,37 @@
+from five import grok
+from zope import schema
+
+from plone.directives import form, dexterity
+
+from plone.app.textfield import RichText
+from plone.namedfile.field import NamedImage
+
+from siyavula.app import _
+
+class IPost(form.Schema):
+    """A Post.
+    """
+    
+    title = schema.TextLine(
+            title=_(u"Title"),
+        )
+    
+    description = schema.Text(
+            title=_(u"A short summary"),
+        )
+    
+    content = RichText(
+            title=_(u"Content"),
+            description=_(u"The actual post"),
+            required=False
+        )
+    
+    image = NamedImage(
+            title=_(u"Image"),
+            description=_(u"An image to display with this item"),
+            required=False
+        )
+
+class View(grok.View):
+    grok.context(IPost)
+    grok.require('zope2.View')
