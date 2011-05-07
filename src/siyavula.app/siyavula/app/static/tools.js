21c3518c0438f5c1dbe946be9ab6ff41906f050f
Johan Beyers <jbeyers@juizi.com>
2011-05-07 21:59:14 +0200

    Banner rotator now waits for all banners to load

--- src/siyavula.app/siyavula/app/static/tools.js
+++ src/siyavula.app/siyavula/app/static/tools.js
@@ -17,6 +17,39 @@ function slideSwitch() {
 
 // end jonraasch
 
+function isImageOk(img) {
+    // During the onload event, IE correctly identifies any images
+    // that weren't downloaded as not complete. Others should too.
+    // Gecko-based browsers act like NS4 in that they report this
+    // incorrectly: they always return true.
+    if (!img.complete) {
+        return false;
+    }
+
+    // However, they do have two very useful properties: naturalWidth
+    // and naturalHeight. These give the true size of the image. If
+    // it failed to load, either of these should be zero.
+    if (typeof img.naturalWidth != "undefined" && img.naturalWidth == 0) {
+        return false;
+    }
+
+    // No other way of checking: assume it's ok.
+    return true;
+}
+
+function checkPreload() {
+    var loadeds = 0;
+    $('.banner img').each(function(index) {
+        if (isImageOk(this)) {
+            loadeds += 1;
+        }
+    });
+    if ( $('.banner').length == loadeds ) {
+        clearInterval(preload_interval_id);
+        setInterval(slideSwitch, 5000 );
+    }
+}
+
 $(document).ready(function()
 {
     // Remove the default text if clicking on the textbox.
@@ -51,5 +84,5 @@ $(document).ready(function()
 
     // Banner rotator
     // Get the set of images and preload.
-    setInterval( "slideSwitch()", 5000 );
+    preload_interval_id = setInterval(checkPreload, 1000);
 });
